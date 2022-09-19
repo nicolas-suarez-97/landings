94b5a3bca69b1005be790cd1392063a5c9995e25
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2022-09-18 23:59:19 -0500

    Added redirect con base path

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-        'media.istockphoto.com',
-        'cdn.pixabay.com',
-        'png.pngtree.com',
-    ],
-  },
+    reactStrictMode: true,
+    images: {
+        domains: [
+            'media.istockphoto.com',
+            'cdn.pixabay.com',
+            'png.pngtree.com',
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/cursos/cocteleria-de-autor',
+                permanent: true,
+            },
+            {
+                source: '/cursos',
+                destination: '/cursos/cocteleria-de-autor',
+                permanent: true,
+            }
+        ]
+    },
 }
+
 
 module.exports = nextConfig
